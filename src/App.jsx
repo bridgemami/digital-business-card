19a591512566c4f001e0e84f3a8bc7cbf7923869
Michael <mdbridgeman@gmail.com>
2023-10-02 09:43:06 -0700

    media queries for bigger screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
+import s from './app.module.css'
 
 function App() {
-
   return (
-    <>
+    <div className={s.app}>
      <Header />
+     <div className={s.body}>
      <Main />
      <Footer />
-    </>
+     </div>
+     </div>
   )
 }
 
